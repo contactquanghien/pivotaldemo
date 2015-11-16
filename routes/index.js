c5e96cbef74d29e744aa7f92cbe64a3c9ae42156
Nguyen Quang Hien <hien@bpohnfileserver.harveynash.vn.local>
2015-11-16 14:49:23 +0700

    check token

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
+  if(req.query.token!=='pivotal'){
+    res.send("Please provide token to access!");
+    return;
+  }
   res.render('index', { title: 'Todo App|Express + MongoDB' });
 };
